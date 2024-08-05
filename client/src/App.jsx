fd61817f1ac095c0e35ce71c6fa0e6cf6407c2ff
webramesh <webdramesh@gmail.com>
2024-08-05 06:44:48 +0545

    pages created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Projects from "./pages/Projects.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+
 export default function App() {
     return(
-        <div>
-           <h1 className='text-3xl text-red-500'>App</h1>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About/>} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/sign-in" element={<SignIn />} />
+                <Route path="/sign-up" element={<SignUp />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
